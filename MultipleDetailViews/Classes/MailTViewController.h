9fd14e1951e74401d6b5cb6f27c49924ee943c62
Ruffat <thomas.pro@ruffat.net>
2014-04-03 14:35:43 +0200

    ok

---
+++ MultipleDetailViews/Classes/MailTViewController.h
@@ -0,0 +1,13 @@
+//
+//  MailTViewController.h
+//  MultipleDetailViews
+//
+//  Created by Thomas on 05/03/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MailTViewController : UIViewController
+
+@end
